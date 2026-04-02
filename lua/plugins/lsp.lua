2b3e390dc4ae1e313c4d5d03230b9a30c936af51
Christopher Eglinton <CEG@easylearn.ch>
2026-04-02 10:37:25 +0200

    Fixed depreciation warning

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    local lspconfig = require("lspconfig")
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
     vim.diagnostic.config({
@@ -19,30 +18,12 @@ return {
       severity_sort = true,
     })
 
-    vim.api.nvim_create_autocmd("CursorHold", {
-        callback = function()
-            vim.diagnostic.open_float(nil, {focus = false, border = "rounded"})
-        end,
-    })
-
     vim.o.updatetime = 250
 
-    lspconfig.gopls.setup({
-      capabilities = capabilities,
-      settings = {
-        gopls = {
-          completeUnimported = true,
-          usePlaceholders = true,
-          gofumpt = true,
-          buildFlags = {
-            "-tags=integration"
-          },
-          analyses = {
-            unusedparams = true,
-          },
-          staticcheck = true,
-        },
-      },
+    vim.api.nvim_create_autocmd("CursorHold", {
+      callback = function()
+        vim.diagnostic.open_float(nil, { focus = false, border = "rounded" })
+      end,
     })
 
     vim.api.nvim_create_autocmd("BufWritePre", {
@@ -53,12 +34,39 @@ return {
       end,
     })
 
-    lspconfig.lua_ls.setup({
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.py",
+      callback = function()
+        vim.lsp.buf.format({ async = false })
+      end,
+    })
+
+    vim.lsp.config("gopls", {
+      capabilities = capabilities,
+      settings = {
+        gopls = {
+          completeUnimported = true,
+          usePlaceholders = true,
+          gofumpt = true,
+          buildFlags = {
+            "-tags=integration",
+          },
+          analyses = {
+            unusedparams = true,
+          },
+          staticcheck = true,
+        },
+      },
+    })
+    vim.lsp.enable("gopls")
+
+    vim.lsp.config("lua_ls", {
+      capabilities = capabilities,
       settings = {
         Lua = {
           runtime = { version = "LuaJIT" },
           diagnostics = {
-            globals = { "vim" }, -- prevent "undefined global 'vim'" warning
+            globals = { "vim" },
           },
           workspace = {
             library = vim.api.nvim_get_runtime_file("", true),
@@ -68,25 +76,34 @@ return {
         },
       },
     })
+    vim.lsp.enable("lua_ls")
 
-    lspconfig.clangd.setup({})
+    vim.lsp.config("clangd", {
+      capabilities = capabilities,
+    })
+    vim.lsp.enable("clangd")
 
-     lspconfig.phpactor.setup({
+    vim.lsp.config("phpactor", {
       cmd = { "phpactor", "language-server" },
       filetypes = { "php" },
-      root_dir = function(fname)
-        local util = require("lspconfig.util")
-        return util.root_pattern("composer.json", ".git")(fname) or vim.fs.dirname(fname)
+      root_dir = function(bufnr, on_dir)
+        local fname = vim.api.nvim_buf_get_name(bufnr)
+        local root =
+          vim.fs.dirname(vim.fs.find({ "composer.json", ".git" }, { upward = true, path = fname })[1])
+          or vim.fs.dirname(fname)
+
+        on_dir(root)
       end,
       capabilities = capabilities,
       settings = {
-        phpactor = {
-        },
+        phpactor = {},
       },
     })
+    vim.lsp.enable("phpactor")
 
-    lspconfig.pylsp.setup({
+    vim.lsp.config("pylsp", {
       cmd = { vim.fn.stdpath("data") .. "/mason/bin/pylsp" },
+      capabilities = capabilities,
       settings = {
         pylsp = {
           plugins = {
@@ -110,14 +127,6 @@ return {
         })
       end,
     })
-
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*.py",
-      callback = function()
-        vim.lsp.buf.format({ async = false })
-      end,
-    })
-
+    vim.lsp.enable("pylsp")
   end,
 }
-
